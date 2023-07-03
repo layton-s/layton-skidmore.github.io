2b746188d6ec9ca4d76f5416685009b759014afc
layton-skidmore <layton.skidmore@gmail.com>
2023-07-03 12:43:08 -0500

    Rounds are working

--- main.js
+++ main.js
@@ -2,70 +2,65 @@ const topLeft = document.querySelector('.top-left-panel');
 const topRight = document.querySelector('.top-right-panel');
 const bottomLeft = document.querySelector('.bottom-left-panel');
 const bottomRight = document.querySelector('.bottom-right-panel');
+const roundElement = document.getElementById('roundNumber');
 
-
+const panels = [topLeft, bottomRight, bottomLeft, topRight];
+const sequence = [getRandomPanel()];
+let sequenceToGuess = [...sequence];
+let canClick = false;
+let currentRound = 1;
 
 function getRandomPanel() {
-    const panels = [
-      topLeft,
-      bottomRight,
-      bottomLeft,
-      topRight,
-    ];
-    return panels[parseInt(Math.random() * panels.length)];
-  }
-  
-  const sequence = [getRandomPanel()];
-  let sequenceToGuess = [...sequence];
-  
-  function flash(panel) {
-    return new Promise(function (resolve) {
-      panel.classList.add('active');
-      setTimeout(function () {
-        panel.classList.remove('active');
-        setTimeout(resolve, 250);
-      }, 1000);
-    });
-  }
-  
-  let canClick = false;
-  
-  function panelClicked(panel) {
-    if (!canClick) return;
-    userFlash(panel);
-    const expectedPanel = sequenceToGuess.shift();
-    if (expectedPanel === panel) {
-      if (sequenceToGuess.length === 0) {
-        // Start new round with a delay
-        canClick = false;
-        setTimeout(function () {
-          sequence.push(getRandomPanel());
-          sequenceToGuess = [...sequence];
-          startFlashing();
-          canClick = true;
-        }, 1500);
-      }
-    } else {
-      // End game
-      alert('Game over');
-    }
-  }
-  
-  async function startFlashing() {
-    canClick = false;
-    for (const panel of sequence) {
-      await flash(panel);
-    }
-    canClick = true;
-  }
-  
-  startFlashing();
-  
-  function userFlash(panel) {
-    panel.className += ' active';
-    setTimeout(function () {
-      panel.className = panel.className.replace(' active', '');
-    }, 500);
-  }
+  return panels[Math.floor(Math.random() * panels.length)];
+}
 
+function flash(panel) {
+  return new Promise(function (resolve) {
+    panel.classList.add('active');
+    setTimeout(function () {
+      panel.classList.remove('active');
+      setTimeout(resolve, 250);
+    }, 1000);
+  });
+}
+
+function panelClicked(panel) {
+  if (!canClick) return;
+  userFlash(panel);
+  const expectedPanel = sequenceToGuess.shift();
+  if (expectedPanel === panel) {
+    if (sequenceToGuess.length === 0) {
+      // Start new round with a delay
+      canClick = false;
+      setTimeout(function () {
+        sequence.push(getRandomPanel());
+        sequenceToGuess = [...sequence];
+        currentRound++;
+        roundElement.textContent = `Round ${currentRound}`;
+        startFlashing();
+        canClick = true;
+      }, 1500);
+    }
+  } else {
+    // End game
+    alert('Game over');
+  }
+}
+
+async function startFlashing() {
+  canClick = false;
+  for (const panel of sequence) {
+    await flash(panel);
+  }
+  canClick = true;
+}
+
+function userFlash(panel) {
+  panel.classList.add('active');
+  setTimeout(function () {
+    panel.classList.remove('active');
+  }, 500);
+}
+
+startFlashing();
            
