20c73734b85c51f87ca6ee13afb936383736e63f
layton-skidmore <layton.skidmore@gmail.com>
2023-07-05 13:56:48 -0500

    implemented Youuu Win/You Lose messages

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const bottomRight = document.querySelector('.bottom-right-panel');
 const roundElement = document.getElementById('roundNumber');
 const startButton = document.getElementById('startButton');
 const playAgainButton = document.getElementById('playAgainButton');
+const youWin = document.querySelector('h1');
+const youLose = document.querySelector('h2');
 const soundEffect = new Audio('https://www.soundjay.com/buttons/sounds/button-3.mp3');
 
 const panels = [topLeft, bottomRight, bottomLeft, topRight];
@@ -13,72 +15,74 @@ let sequenceToGuess = [];
 let canClick = false;
 let currentRound = 1;
 
+playAgainButton.style.display = 'none';
+youWin.style.display = 'none';
+youLose.style.display = 'none';
+
 function getRandomPanel() {
   return panels[Math.floor(Math.random() * panels.length)];
 }
 
-async function flash(panel) {
-  return new Promise(function (resolve) {
-    panel.classList.add('active');
-    soundEffect.play(); 
-    setTimeout(function () {
-      panel.classList.remove('active');
-      setTimeout(resolve, 250);
-    }, 1000);
-  });
-}
-
 async function startFlashing() {
-  canClick = false;
-  await new Promise(function (resolve) {
-    setTimeout(resolve, 700);
-  });
-
-  const sequenceLength = sequence.length;
-  for (let i = 0; i < sequenceLength; i++) {
-    const panel = sequence[i];
-    await flash(panel);
+    canClick = false; // Disable clicking temporarily
+    const sequenceLength = sequence.length; // Get the length of the sequence
+    for (let i = 0; i < sequenceLength; i++) {
+      const panel = sequence[i]; // Get the panel at the current index
+      panel.classList.add('active'); // Add the 'active' class to the panel
+      soundEffect.play(); // Play the sound effect
+      await new Promise(function (resolve) {
+        setTimeout(function () {
+          resolve();
+        }, 1000);
+      }); // Wait for 1 second
+      panel.classList.remove('active'); // Remove the 'active' class from the panel
+      await new Promise(function (resolve) {
+        setTimeout(function () {
+          resolve();
+        }, 250);
+      }); // Wait for 250 milliseconds
+    }
+    canClick = true; // Allow clicking again
   }
-
-  canClick = true;
-}
 
 function userFlash(panel) {
   panel.classList.add('active');
-  soundEffect.play(); 
+  soundEffect.play();
   setTimeout(function () {
     panel.classList.remove('active');
   }, 500);
 }
 
 function panelClicked(panel) {
-  if (!canClick) return;
-  userFlash(panel);
-  const expectedPanel = sequenceToGuess.shift();
-
-  if (expectedPanel === panel) {
-    if (sequenceToGuess.length === 0) {
-      if (currentRound === 10) {
-        endGame(true);
-        return;
+    if (!canClick) return; // Check if clicking is currently allowed
+    userFlash(panel); // Flash the panel clicked by the user
+    const expectedPanel = sequenceToGuess.shift(); // Get the expected panel from the sequence to guess
+    if (expectedPanel === panel) { // Check if the clicked panel matches the expected panel
+      if (sequenceToGuess.length === 0) { // Check if the entire sequence has been guessed
+        if (currentRound === 4) { // Check if it's the final round of the game
+          endGame(true); // End the game with a win
+          return;
+        }
+        canClick = false; // Disable clicking temporarily
+        setTimeout(function () {
+          sequence.push(getRandomPanel()); // Generate a new random panel and add it to the sequence
+          sequenceToGuess = [...sequence]; // Update the sequence to guess with the new sequence
+          currentRound++; // Increment the current round counter
+          roundElement.textContent = `Round ${currentRound}`; // Update the round display element with the current round number
+          startFlashing(); // Start flashing the new sequence
+          canClick = true; // Allow clicking again
+        }, 1500);
       }
-      canClick = false;
-      setTimeout(function () {
-        sequence.push(getRandomPanel());
-        sequenceToGuess = [...sequence];
-        currentRound++;
-        roundElement.textContent = `Round ${currentRound}`;
-        startFlashing();
-        canClick = true;
-      }, 1500);
+    } else {
+      endGame(false); // End the game with a loss if the clicked panel does not match the expected panel
     }
-  } else {
-    endGame(false);
   }
-}
 
 function startGame() {
   startButton.style.display = 'none';
+  youWin.style.display = 'none';
+  youLose.style.display = 'none';
+
   sequence.push(getRandomPanel());
   sequenceToGuess = [...sequence];
   roundElement.textContent = `Round ${currentRound}`;
@@ -88,9 +92,9 @@ function startGame() {
 
 function endGame(playerWon) {
   if (playerWon) {
-    alert('Congratulations, you won!');
+    youWin.style.display = 'block';
   } else {
-    alert('Game over');
+    youLose.style.display = 'block';
   }
   playAgainButton.style.display = 'block';
   canClick = false;
@@ -98,6 +102,9 @@ function endGame(playerWon) {
 
 function resetGame() {
   playAgainButton.style.display = 'none';
+  youWin.style.display = 'none';
+  youLose.style.display = 'none';
+
   sequence.length = 0;
   sequenceToGuess.length = 0;
   currentRound = 1;
@@ -111,4 +118,4 @@ function resetGame() {
   }, 1000);
 }
 
-playAgainButton.style.display = 'none';
+
